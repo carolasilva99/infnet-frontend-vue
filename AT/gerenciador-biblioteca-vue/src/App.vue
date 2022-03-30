f9a2c6509a56da25b20bdba1cbc836dbb3ded08c
Carol Alves <carol.alves99@live.com>
2022-03-30 07:55:22 -0300

    Adiciona header da aplicação

--- AT/gerenciador-biblioteca-vue/src/App.vue
+++ AT/gerenciador-biblioteca-vue/src/App.vue
@@ -1,8 +1,27 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "@/components/HelloWorld.vue";
 </script>
 
 <template>
-  
+  <div id="app">
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <router-link to="/" class="navbar-brand">Gerenciador de Biblioteca</router-link>
+      <div class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link to="/livros" class="nav-link">Livros</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/adicionar" class="nav-link">Adicionar</router-link>
+        </li>
+      </div>
+    </nav>
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  </div>
 </template>
+<script>
+export default {
+  name: "app",
+};
+</script>
